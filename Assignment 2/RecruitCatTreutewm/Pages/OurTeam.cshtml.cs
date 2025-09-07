a1787dc1308c1abe5dedfce52e7f1742c2b6d51a
Liam Treutel <Liam Treutel>
2025-09-06 22:59:47 -0700

    Create the site, lay the grounwork.

---
+++ Assignment 2/RecruitCatTreutewm/Pages/OurTeam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatTreutewm.Pages
+{
+    public class OurTeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
