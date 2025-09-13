14e2e9f2c2c77e31a270151bf44e4bb5c4cdf393
Liam Treutel <Liam Treutel>
2025-09-13 15:44:05 -0700

    Split the models file (which was combined for the sake of the assignment screenshot)

--- Week3/Week3_BankAccount/Models/BankAccount.cs
+++ Week3/Week3_BankAccount/Models/BankAccount.cs
@@ -48,39 +48,4 @@ namespace Week3_BankAccount.Models
             this.AccountholderID = newAccountholder;
         }
     }
-
-    public class AccountHolder
-    {
-        private static int s_accountNumberSeed = 1234567890;
-        [Key]
-        public string IDNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateOnly DateOfBirth { get; set; }
-        public bool IsCriminal { get; set; }
-        public int IRSReportingNumber { get; set; }
-
-
-        public AccountHolder()
-        {
-            this.IDNumber = s_accountNumberSeed.ToString("000000000000");
-            s_accountNumberSeed++;
-
-            this.IsCriminal = false; 
-            this.IRSReportingNumber = 0; // see above
-        }
-
-        public AccountHolder(string firstName, string lastName, DateOnly dateOfBirth, bool isCriminal = false)
-        {
-            this.FirstName = firstName;
-            this.LastName = lastName;
-            this.DateOfBirth = dateOfBirth;
-            this.IDNumber = s_accountNumberSeed.ToString("000000000000");
-            s_accountNumberSeed++;
-
-            if (isCriminal) { throw new Exception("Error: Do not commit fraud, please."); }
-            this.IsCriminal = isCriminal;
-            this.IRSReportingNumber = 0; // see above
-        }
-    }
 }
