5f5ae8f796d3e2814b584a5042d1897f99930e62
Liam Treutel <Liam Treutel>
2025-09-26 16:14:38 -0700

    Initial push

---
+++ Assignment 5/IS7012_Ex5/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IS7012_Ex5.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
